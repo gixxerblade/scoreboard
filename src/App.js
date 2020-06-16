c81f4fdb9894cf8b03d9260a1d8b2f0609f749e4
gixxerblade <sdclarkie@gmail.com>
2020-06-15 22:29:16 -0400

    Coding challenge 6-15-2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const App = () => {
+  const initialTime = 15;
+  const initialState = 0;
+  const [homeScore, setHomeScore] = useState(initialState);
+  const [visitorScore, setVisitorScore] = useState(initialState);
+  const [countdown, setCountdown] = useState(initialTime);
+  const reset = () => {
+    setHomeScore(initialState);
+    setVisitorScore(initialState);
+    setCountdown(initialTime);
+  };
+  useEffect(() => {
+    const timer =
+      countdown > 0 && setTimeout(() => setCountdown(countdown - 1), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [countdown]);
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="scoreboard-bg">
+        <div className="timer-container">
+          <div className="timer">00:{countdown}</div>
+          <div className="text">Time</div>
+        </div>
+        <div className="home-visitor-container">
+          <div className="home-container">
+            <div className="home">{homeScore}</div>
+            <div className="text">Home</div>
+            <div
+              onClick={() =>
+                setHomeScore(homeScore <= 0 ? initialState : homeScore - 1)
+              }
+              className="minus"
+            >
+              -
+            </div>
+            <div onClick={() => setHomeScore(homeScore + 1)} className="plus">
+              +
+            </div>
+          </div>
+          <div className="visitor-container">
+            <div
+              onClick={() => setVisitorScore(visitorScore + 1)}
+              className="visitor"
+            >
+              {visitorScore}
+            </div>
+            <div className="text">Visitor</div>
+            <div
+              onClick={() =>
+                setVisitorScore(
+                  visitorScore <= 0 ? initialState : visitorScore - 1
+                )
+              }
+              className="minus"
+            >
+              -
+            </div>
+            <div
+              onClick={() => setVisitorScore(visitorScore + 1)}
+              className="plus"
+            >
+              +
+            </div>
+          </div>
+        </div>
+        <button onClick={reset} className="reset">
+          Reset
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
